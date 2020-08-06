709174eabf3a3255343776af88c75af5d3c70db6
kelvin lanier <kelvinlanier@yahoo.com>
2020-08-06 22:50:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,3 @@
-class CashRegister
-
-  attr_accessor :total, :discount, :last_transaction_amount, :items
-
-  def initialize(discount = 0)
-    @total = 0
-    @discount = discount
-    @items = []
-  end
 
 
+  
